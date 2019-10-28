257a1c8ec861fd311824f87804561f784bf4392a
Fischer Jemison <jemisonf@oregonstate.edu>
2019-10-28 13:35:24 -0700

    adds proxyquire and additional test for getDoughs return

--- src/tests/unit/doughs-dao.test.js
+++ src/tests/unit/doughs-dao.test.js
@@ -1,11 +1,11 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 
-import * as connectionApi from 'api/v1/db/oracledb/connection';
-import * as doughsSerializer from 'api/v1/serializers/doughs-serializer';
-import { getDoughs } from 'api/v1/db/oracledb/doughs-dao';
+// import * as connectionApi from 'api/v1/db/oracledb/connection';
+// import * as doughsSerializer from 'api/v1/serializers/doughs-serializer';
 import { getDoughsData } from './test-data';
 
 chai.should();
@@ -16,32 +16,40 @@ describe('test doughs dao', () => {
   let serializeDoughsStub;
   let getConnectionStub;
   let connectionSpy;
+  let doughsDao;
   beforeEach(() => {
     connectionSpy = sinon.stub().returns(getDoughsData.executeReturn);
-    serializeDoughsStub = sinon.stub(doughsSerializer, 'serializeDoughs');
-    getConnectionStub = sinon.stub(connectionApi, 'getConnection');
-    getConnectionStub.resolves({
+    serializeDoughsStub = sinon.stub().returns(getDoughsData.baseGetDoughsReturn);
+    getConnectionStub = sinon.stub().resolves({
       execute: connectionSpy,
       close: () => { },
       commit: () => { },
     });
-    serializeDoughsStub.returns(getDoughsData.baseGetDoughsReturn);
+
+    doughsDao = proxyquire('../../api/v1/db/oracledb/doughs-dao', {
+      './connection': {
+        getConnection: getConnectionStub,
+      },
+      '../../serializers/doughs-serializer': {
+        serializeDoughs: serializeDoughsStub,
+      },
+    });
   });
   afterEach(() => {
     sinon.restore();
   });
   describe('getDoughs', () => {
     it('returns the output from serializeDoughs', () => {
-      const result = getDoughs({});
+      const result = doughsDao.getDoughs({});
       return result.should.eventually.deep.equal(getDoughsData.baseGetDoughsReturn);
     });
     it('extracts only the rows from the database return', async () => {
-      await getDoughs({});
+      await doughsDao.getDoughs({});
       serializeDoughsStub.getCall(0).calledWith(getDoughsData.executeReturnRows).should.be.true;
     });
     describe('when it has an invalid filter', () => {
       it('does not parse the invalid filter', async () => {
-        await getDoughs(getDoughsData.invalidFilters);
+        await doughsDao.getDoughs(getDoughsData.invalidFilters);
         connectionSpy
           .getCall(0)
           .should.have.been
@@ -52,8 +60,12 @@ describe('test doughs dao', () => {
       });
     });
     describe('when it has valid filters', () => {
+      it('still returns the output from serializeDoughs', () => {
+        const result = doughsDao.getDoughs(getDoughsData.waterTempFilter);
+        return result.should.eventually.deep.equal(getDoughsData.baseGetDoughsReturn);
+      });
       it('properly parses those filters into bind paramters', async () => {
-        await getDoughs(getDoughsData.waterTempFilter);
+        await doughsDao.getDoughs(getDoughsData.waterTempFilter);
         connectionSpy
           .getCall(0)
           .should.have.been
@@ -65,7 +77,7 @@ describe('test doughs dao', () => {
     });
     describe('when it gets valid and invalid filters', () => {
       it('only parses the valid filters into bind parameters', async () => {
-        await getDoughs(getDoughsData.mixedValidFilters);
+        await doughsDao.getDoughs(getDoughsData.mixedValidFilters);
         connectionSpy
           .getCall(0)
           .should.have.been
@@ -77,7 +89,7 @@ describe('test doughs dao', () => {
     });
     describe('when it gets multiple valid filters', () => {
       it('properly parses all of those filters', async () => {
-        await getDoughs(getDoughsData.multipleFilters);
+        await doughsDao.getDoughs(getDoughsData.multipleFilters);
         connectionSpy
           .getCall(0)
           .calledWith(
